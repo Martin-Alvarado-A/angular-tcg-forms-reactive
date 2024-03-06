6de2e74b656ddf597b5c4dbc34c753f4ddd40b84
martin <martin.alvarado2002@gmail.com>
2024-03-06 12:28:56 +0100

    [15-209]: Reactive: Submitting the Form

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,4 +17,8 @@ export class AppComponent implements OnInit {
       gender: new FormControl('male'),
     });
   }
+
+  onSubmit() {
+    console.log(`🔎 | AppComponent | onSubmit > signupForm:`, this.signupForm);
+  }
 }
